58787b6ff704c3498ea608ded9bd7597b22b6e94
Carlos Roig <carroisal3@gmail.com>
2017-10-26 18:46:49 +0200

    Initial Commit

---
+++ FireBaseLoginRegister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseLoginRegister
+//
+//  Created by Carlos Roig on 26/10/17.
+//  Copyright © 2017 CRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
