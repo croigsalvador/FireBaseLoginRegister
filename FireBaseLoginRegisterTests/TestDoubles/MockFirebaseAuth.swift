b7942e36ad0ad7a1ddaf44d725d625d7f1b22ccb
Carlos Roig <carroisal3@gmail.com>
2017-11-08 20:03:06 +0100

    Removed

--- FireBaseLoginRegisterTests/TestDoubles/MockFirebaseAuth.swift
+++ FireBaseLoginRegisterTests/TestDoubles/MockFirebaseAuth.swift
@@ -10,35 +10,35 @@ import Foundation
 import Firebase
 import FirebaseAuth
 
-class MockFirebaseAuth: Auth {
- 
-    var error: Error?
-    var user: User?
-    
-    override class func auth() -> MockFirebaseAuth {
-        FirebaseApp.configure()
-        return MockFirebaseAuth()
-    }
-    
-    override func createUser(withEmail email: String, password: String, completion: AuthResultCallback? = nil) {
-        completion!(user, error)
-    }
-    
-    deinit {
-        FirebaseApp.app()?.delete({ (success) in
-        
-        })
-    }
-    
-}
-
-class FakeFirebaseUser: User {
-    override var uid: String {
-        return "Fake uid"
-    }
-    
-    override var email: String? {
-        return "fake@email.com"
-    }
-}
+//class MockFirebaseAuth: Auth {
+// 
+//    var error: Error?
+//    var user: User?
+//    
+//    override class func auth() -> MockFirebaseAuth {
+//        FirebaseApp.configure()
+//        return MockFirebaseAuth()
+//    }
+//    
+//    override func createUser(withEmail email: String, password: String, completion: AuthResultCallback? = nil) {
+//        completion!(user, error)
+//    }
+//    
+//    deinit {
+//        FirebaseApp.app()?.delete({ (success) in
+//        
+//        })
+//    }
+//    
+//}
+//
+//class FakeFirebaseUser: User {
+//    override var uid: String {
+//        return "Fake uid"
+//    }
+//    
+//    override var email: String? {
+//        return "fake@email.com"
+//    }
+//}
 
--- FireBaseLoginRegisterTests/Tests/SessionUserNetworkProviderTests.swift
+++ FireBaseLoginRegisterTests/Tests/SessionUserNetworkProviderTests.swift
@@ -10,165 +10,165 @@ import XCTest
 @testable import FireBaseLoginRegister
 
 class SessionUserNetworkProviderTests: BaseExpectationTest {
-    
-    var sut: SessionUserNetworkProvider!
-    var mockDatabase: MockFirebaseDatabase!
-    var mockAuth: MockFirebaseAuth!
-    let fakeUser = FakeFirebaseUser()
-    var stubReference = StubFirebaseDatabaseReference()
-    
-    let loginParams = LoginUserParams(email:"email@gmail.com", password:"12345")
-    let registerParams = RegisterUserBasicParams(name:"Name",email:"email@gmail.com", password:"12345")
-    let error = NSError(domain:"", code:4, userInfo:nil)
-    
-    override func setUp() {
-        super.setUp()
-        mockDatabase = MockFirebaseDatabase()
-        mockDatabase.stubReference = stubReference
-        mockAuth = MockFirebaseAuth.auth()
-        sut = SessionUserFirebaseProvider.init(mockDatabase, mockAuth)
-        expectation  = self.expectation(description: "Expecting Google connect returns user")
-
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-        expectation = nil
-        mockDatabase.stubReference = nil
-        mockAuth = nil
-        mockDatabase = nil
-        sut = nil
- 
-    }
-
-    private func checkErrorIn(_ result: RegisterResult) {
-        switch result {
-        case .success(_):
-            break
-        case let .failure(error):
-            XCTAssertNotNil(error)
-        }
-        self.expectation.fulfill()
-    }
-    
-    private func checkSuccessIn(_ result: RegisterResult,_ params:RegisterUserBasicParams) {
-        switch result{
-        case let .success(user):
-            XCTAssertTrue(user?.name == params.name)
-            XCTAssertTrue(user?.email == params.email)
-            break
-        case .failure(_):
-            break
-        }
-        expectation.fulfill()
-    }
-    
-    func testRegister_withRegisterParamsShouldReturnErrorForSomethingWroinAuthentication() {
-        mockAuth.error = error
-        sut.register(registerParams) { (result) in
-            self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testRegister_withRegisterParamsShouldReturnErrorFromUpdatingUser() {
-        mockAuth.user = fakeUser
-        stubReference.error = error
-        sut.register(registerParams) { (result) in
-            self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-        
-    }
-    
-    func testRegister_withRegisterParamsShouldUserSessionCreatedWithSameEmail() {
-        var params = registerParams
-        params.email = fakeUser.email!
-        mockAuth.user = fakeUser
-        sut.register(params) { (result) in
-            self.checkSuccessIn(result, params)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testLogin_withLoginParamsShouldReturnError() {
-        mockAuth.error =  error
-        sut.loginUser(loginParams) { (result) in
-           self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testLogin_withLoginParamsShouldReturnErrorFromUpdatingUserInServer() {
-        mockAuth.user = fakeUser
-        stubReference.error = error
-        sut.loginUser(loginParams) { (result) in
-           self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testLogin_withLoginParamsShouldUserSessionCreatedWithSameEmail() {
-        let params = LoginUserParams(email:fakeUser.email!, password:"12345")
-        mockAuth.user = fakeUser
-        sut.loginUser(params) { (result) in
-            switch result {
-            case let .success(user):
-                XCTAssertTrue(user?.email == params.email)
-                break
-            case .failure(_):
-                break
-            }
-            self.expectation.fulfill()
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testRegister_withSocialRequestForGoogleShouldReturnError() {
-        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .google)
-        mockAuth.error =  error
-        sut.register(socialRequest) { (result) in
-            self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testRegister_withSocialRequestForFacebookShouldReturnError() {
-        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
-        mockAuth.error =  error
-        sut.register(socialRequest) { (result) in
-            self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testRegister_withSocialRequestShouldReturnErrorFromUpdatingUserInServer() {
-        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
-
-        mockAuth.user = fakeUser
-        stubReference.error = error
-        sut.register(socialRequest) { (result) in
-            self.checkErrorIn(result)
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
-    func testRegister_withSocialRequestShouldUserSessionCreatedWithSameEmail() {
-        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
-
-        mockAuth.user = fakeUser
-        sut.register(socialRequest) { (result) in
-            switch result {
-            case let .success(user):
-                XCTAssertTrue(user?.email == socialRequest.email)
-                XCTAssertTrue(user?.token == socialRequest.token)
-                break
-            case .failure(_):
-                break
-            }
-            self.expectation.fulfill()
-        }
-        waitForExpectation(with: 1.0)
-    }
-    
+//    
+//    var sut: SessionUserNetworkProvider!
+//    var mockDatabase: MockFirebaseDatabase!
+////    var mockAuth: MockFirebaseAuth!
+//    let fakeUser = FakeFirebaseUser()
+//    var stubReference = StubFirebaseDatabaseReference()
+//    
+//    let loginParams = LoginUserParams(email:"email@gmail.com", password:"12345")
+//    let registerParams = RegisterUserBasicParams(name:"Name",email:"email@gmail.com", password:"12345")
+//    let error = NSError(domain:"", code:4, userInfo:nil)
+//    
+//    override func setUp() {
+//        super.setUp()
+//        mockDatabase = MockFirebaseDatabase()
+//        mockDatabase.stubReference = stubReference
+//        mockAuth = MockFirebaseAuth.auth()
+//        sut = SessionUserFirebaseProvider.init(mockDatabase, mockAuth)
+//        expectation  = self.expectation(description: "Expecting Google connect returns user")
+//
+//    }
+//    
+//    override func tearDown() {
+//        super.tearDown()
+//        expectation = nil
+//        mockDatabase.stubReference = nil
+//        mockAuth = nil
+//        mockDatabase = nil
+//        sut = nil
+// 
+//    }
+//
+//    private func checkErrorIn(_ result: RegisterResult) {
+//        switch result {
+//        case .success(_):
+//            break
+//        case let .failure(error):
+//            XCTAssertNotNil(error)
+//        }
+//        self.expectation.fulfill()
+//    }
+//    
+//    private func checkSuccessIn(_ result: RegisterResult,_ params:RegisterUserBasicParams) {
+//        switch result{
+//        case let .success(user):
+//            XCTAssertTrue(user?.name == params.name)
+//            XCTAssertTrue(user?.email == params.email)
+//            break
+//        case .failure(_):
+//            break
+//        }
+//        expectation.fulfill()
+//    }
+//    
+//    func testRegister_withRegisterParamsShouldReturnErrorForSomethingWroinAuthentication() {
+//        mockAuth.error = error
+//        sut.register(registerParams) { (result) in
+//            self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testRegister_withRegisterParamsShouldReturnErrorFromUpdatingUser() {
+//        mockAuth.user = fakeUser
+//        stubReference.error = error
+//        sut.register(registerParams) { (result) in
+//            self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//        
+//    }
+//    
+//    func testRegister_withRegisterParamsShouldUserSessionCreatedWithSameEmail() {
+//        var params = registerParams
+//        params.email = fakeUser.email!
+//        mockAuth.user = fakeUser
+//        sut.register(params) { (result) in
+//            self.checkSuccessIn(result, params)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testLogin_withLoginParamsShouldReturnError() {
+//        mockAuth.error =  error
+//        sut.loginUser(loginParams) { (result) in
+//           self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testLogin_withLoginParamsShouldReturnErrorFromUpdatingUserInServer() {
+//        mockAuth.user = fakeUser
+//        stubReference.error = error
+//        sut.loginUser(loginParams) { (result) in
+//           self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testLogin_withLoginParamsShouldUserSessionCreatedWithSameEmail() {
+//        let params = LoginUserParams(email:fakeUser.email!, password:"12345")
+//        mockAuth.user = fakeUser
+//        sut.loginUser(params) { (result) in
+//            switch result {
+//            case let .success(user):
+//                XCTAssertTrue(user?.email == params.email)
+//                break
+//            case .failure(_):
+//                break
+//            }
+//            self.expectation.fulfill()
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testRegister_withSocialRequestForGoogleShouldReturnError() {
+//        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .google)
+//        mockAuth.error =  error
+//        sut.register(socialRequest) { (result) in
+//            self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testRegister_withSocialRequestForFacebookShouldReturnError() {
+//        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
+//        mockAuth.error =  error
+//        sut.register(socialRequest) { (result) in
+//            self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testRegister_withSocialRequestShouldReturnErrorFromUpdatingUserInServer() {
+//        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
+//
+//        mockAuth.user = fakeUser
+//        stubReference.error = error
+//        sut.register(socialRequest) { (result) in
+//            self.checkErrorIn(result)
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
+//    func testRegister_withSocialRequestShouldUserSessionCreatedWithSameEmail() {
+//        let socialRequest = RequestSocialRequestModel.init(idToken:"", token: "Social request token", name: "User", email: "email@test.com", loginType: .facebook)
+//
+//        mockAuth.user = fakeUser
+//        sut.register(socialRequest) { (result) in
+//            switch result {
+//            case let .success(user):
+//                XCTAssertTrue(user?.email == socialRequest.email)
+//                XCTAssertTrue(user?.token == socialRequest.token)
+//                break
+//            case .failure(_):
+//                break
+//            }
+//            self.expectation.fulfill()
+//        }
+//        waitForExpectation(with: 1.0)
+//    }
+//    
 }
